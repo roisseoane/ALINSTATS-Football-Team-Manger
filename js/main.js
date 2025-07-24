716e10925c8f757b4e045383a741747dd56f8628
roisseoane <65539830+roisseoane@users.noreply.github.com>
2025-07-24 03:28:37 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -70,6 +70,7 @@ export function inicializarUIPrincipal() {
     elements.nav.btnEstadistiques.addEventListener('click', () => activarTab('estadistiques'));
     elements.nav.btnAlineacio.addEventListener('click', () => activarTab('alineacio'));
     elements.nav.btnClips.addEventListener('click', () => activarTab('clips'));
+    elements.nav.btnConfiguracion.addEventListener('click', () => activarTab('configuracion'));
     elements.modal.backdrop.addEventListener('click', cerrarModal);
 
     if (elements.togglePizarraBtn) {
